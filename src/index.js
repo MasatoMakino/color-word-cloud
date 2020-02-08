40bd9005201f2260077a07c664053dc798b2ef02
MasatoMakino <unagiinu.g@gmail.com>
2020-02-08 16:38:03 +0900

    修正 : return抜け

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import * as WordCloud from "wordcloud";
 
-WordCloud(document.getElementById("cloud_canvas"), {
-  list: getList()
-});
-
 const getList = () => {
-  [
+  return [
     ["foo", 64],
     ["bar", 48]
   ];
 };
+
+WordCloud(document.getElementById("cloud_canvas"), {
+  list: getList()
+});
