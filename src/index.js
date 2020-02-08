0f2c7aff7f136294bfb5150623907bde815089fd
MasatoMakino <unagiinu.g@gmail.com>
2020-02-08 16:31:29 +0900

    修正 : リスト取得を関数化

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import * as WordCloud from "wordcloud";
+import { getMaxListeners } from "cluster";
 
 WordCloud(document.getElementById("cloud_canvas"), {
-  list: [
+  list: getList()
+});
+
+const getList = () => {
+  [
     ["foo", 64],
     ["bar", 48]
-  ]
-});
+  ];
+};
