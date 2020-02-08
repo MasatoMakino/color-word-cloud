7c3dd8c5d5da4f804eac4a857770dd46383b4f27
MasatoMakino <unagiinu.g@gmail.com>
2020-02-08 16:25:13 +0900

    追加 : ワードクラウドの実行
    データは決め打ち。実行できているかの確認用。

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import * as WordCloud from "wordcloud";
+
+WordCloud(document.getElementById("cloud_canvas"), {
+  list: [
+    ["foo", 64],
+    ["bar", 48]
+  ]
+});
+
 console.log("hello world");
