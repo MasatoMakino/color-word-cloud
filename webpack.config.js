e5267a78daf1dd0a415b81d73b90a7ab0f51e8df
MasatoMakino <unagiinu.g@gmail.com>
2020-02-08 14:42:25 +0900

    追加 : webpackの最小構成ファイル

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: `${__dirname}/docs`,
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
