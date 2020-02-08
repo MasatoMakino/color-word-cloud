ab7abfffe9837c36a50accc2366c474d30de9416
MasatoMakino <unagiinu.g@gmail.com>
2020-02-08 22:35:19 +0900

    追加 : スプレッドシート情報の読み込み

--- src/index.js
+++ src/index.js
@@ -11,6 +11,20 @@ const getColor = () => {
   return "#ff00ff";
 };
 
+const url =
+  "https://spreadsheets.google.com/feeds/list/1uGluGMd7v8Dujez_u903wKAvtufwCLL178AYL9bdpOY/1/public/values?alt=json";
+
+fetch(url, {})
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+    console.log(json.feed.entry);
+  })
+  .catch(function(err1) {
+    console.log("err=" + err1);
+  });
+
 WordCloud(document.getElementById("cloud_canvas"), {
   list: getList(),
   color: getColor
