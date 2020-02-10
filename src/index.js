c547adec6c3fe92405a4b9109cbfc8faebb3a253
MasatoMakino <unagiinu.g@gmail.com>
2020-02-10 12:51:49 +0900

    修正 : トレンドjsonとスプレッドシートの色情報を合成。

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import * as WordCloud from "wordcloud";
-import { load, getColor, getList } from "./SheetLoader";
+import { load, getColor } from "./SheetLoader";
+import { loadTrends, getList } from "./ListLoader";
 
 async function main() {
   await load();
+  await loadTrends();
+
   WordCloud(document.getElementById("cloud_canvas"), {
-    list: getList(),
-    color: getColor
+    list: getList(0.2),
+    color: getColor,
+    backgroundColor: "#444",
+    minSize: 1.0
   });
 }
 main();
